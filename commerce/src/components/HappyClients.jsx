c709db49947e5d10cbd1c29fb33a61e76db06192
Vivekkrguptajio <vivekkrguptajio@gmail.com>
2026-07-31 22:57:09 +0530

    style: change testimonial star ratings fill to brand orange #FD5800

--- commerce/src/components/HappyClients.jsx
+++ commerce/src/components/HappyClients.jsx
@@ -135,7 +135,7 @@ const HappyClients = () => {
                 {/* Stars */}
                 <div className="flex gap-[3px] mb-4">
                   {[1, 2, 3, 4, 5].map((star) => (
-                    <svg key={star} width="16" height="16" viewBox="0 0 24 24" fill="#f2ff00" stroke="black" strokeWidth="1.5">
+                    <svg key={star} width="16" height="16" viewBox="0 0 24 24" fill="#FD5800" stroke="#FD5800" strokeWidth="1">
                       <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
                     </svg>
                   ))}
